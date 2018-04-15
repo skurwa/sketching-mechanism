f2e38e7677274c0616b4ec101e799fa5263dfc4f
Siddharth Kurwa <skurwa@utexas.edu>
2018-04-15 17:33:36 -0500

    Tweaks to boundary generation and position data text file porting

--- Image Processing/boundaryGeneration.m
+++ Image Processing/boundaryGeneration.m
@@ -1,10 +1,14 @@
-I = imread('test_images/Longhorn.png');
+
+filename = 'Longhorn.png';
+strcat(filename)
+
+I = imread(strcat('test_images/', filename));
 
 BW = im2bw(I);
 BW = imcomplement(BW);
 dim = size(BW);
 
-nominalCamRad = 2.5;
+centerCamRad = 100;
 
 i = 1; j = 1;
 noSeed = true;
@@ -20,46 +24,71 @@ end
 
 boundary = bwtraceboundary(BW,[seedY,seedX],'E');
 
+figure(1);
 imshow(I)
 hold on;
 plot(boundary(:,2),boundary(:,1),'g','LineWidth',3);
 hold off;
 
+singleSideDel = centerCamRad - 1;
 aspectRatio = (max(boundary(:,2)) - min(boundary(:,2))) / (max(boundary(:,1)) - min(boundary(:,1)));
-normXdelRad = ((boundary(:,2) - min(boundary(:,2)))* aspectRatio / (max(boundary(:,2)) - min(boundary(:,2))))- (.5*aspectRatio);
-normYdelRad = ((boundary(:,1) - min(boundary(:,1)))/ (max(boundary(:,1)) - min(boundary(:,1)))) - .5;
+if aspectRatio >= 1 % this means that the x distance is larger than the y
+    % normalize x distance to range between [-1,1]
+    xDelRad = (((boundary(:,2) - min(boundary(:,2))) * 2 * singleSideDel / (max(boundary(:,2)) - min(boundary(:,2))))- singleSideDel);
+    % normalize y distance to range between [-1,1] but then also make
+    % proportional to x range via the aspect ratio
+    yDelRad = (((boundary(:,1) - min(boundary(:,1))) * 2 * singleSideDel / (max(boundary(:,1)) - min(boundary(:,1)))) - singleSideDel)/aspectRatio;
+else
+    % normalize x distance to range between [-1,1] but then also make
+    % proportional to y range via the aspect ratio
+    xDelRad = (((boundary(:,2) - min(boundary(:,2)))*2*singleSideDel * aspectRatio / (max(boundary(:,2)) - min(boundary(:,2))))- singleSideDel);
+    % normalize y distance to range between [-1,1] 
+    yDelRad = (((boundary(:,1) - min(boundary(:,1))) * 2 * singleSideDel / (max(boundary(:,1)) - min(boundary(:,1)))) - singleSideDel);
+end
 
-dim = size(normXdelRad);
+dim = size(xDelRad);
 
 theta = zeros(1,dim(1));
 for ii = 1:1:dim(1)
     theta(ii) = (ii-1)*(360/dim(1));
 end
 
-figure(1);
-plot(theta, normXdelRad)
-xlabel('degrees');
-ylabel('normalized x radius')
 figure(2);
-plot(theta, normYdelRad)
+subplot(1,2,1);
+plot(theta, xDelRad)
 xlabel('degrees');
-ylabel('normalized y radius')
+ylabel('delta x cam radius');
+title(strcat(filename, '-change in x cam radius vs theta'));
+subplot(1,2,2);
+plot(theta, yDelRad)
+xlabel('degrees');
+ylabel('delta y cam radius');
+title(strcat(filename, '-change in y cam radius vs theta'));
 
 % convert to cartesian coordinates
-xCamRad = nominalCamRad + normXdelRad';
+xCamRad = centerCamRad + xDelRad';
 xRad_xPos = xCamRad .* cosd(theta);  
 xRad_yPos = xCamRad .* sind(theta);
 
-yCamRad = nominalCamRad + normYdelRad';
+yCamRad = centerCamRad + yDelRad';
 yRad_xPos = yCamRad .* cosd(theta);
 yRad_yPos = yCamRad .* sind(theta);
 
 figure(3);
+subplot(1,2,1);
 plot(xRad_xPos, xRad_yPos);
-
-figure(4);
+title(strcat(filename, '-xCamShape'));
+xlabel('x position (in)');
+ylabel('y posiiton (in)');
+subplot(1,2,2);
 plot(yRad_xPos, yRad_yPos);
+title(strcat(filename, '-yCamShape'));
+xlabel('x position (in)');
+ylabel('y posiiton (in)');
 
-A = [xRad_xPos', xRad_yPos', zeros(length(xRad_xPos), 1)];
+% porting to text file in Solidworks-readable format
+xCamShape = [xRad_xPos', xRad_yPos', zeros(length(xRad_xPos), 1)];
+yCamShape = [yRad_xPos', yRad_yPos', zeros(length(yRad_xPos), 1)];
 
-save('circle xCam.txt', 'A', '-ascii', '-double', '-tabs')
+save(strcat(filename, '-xCam.txt'), 'xCamShape', '-ascii', '-double', '-tabs')
+save(strcat(filename, '-yCam.txt'), 'yCamShape', '-ascii', '-double', '-tabs')
